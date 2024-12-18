e82d6a262b17a1058ef4768fc2eb49a31aa6b915
neyen <elise.neyens@telecomnancy.eu>
2024-12-18 16:53:21 -0500

    ESSAI 2

--- utils/create_model_checkpoint.py
+++ utils/create_model_checkpoint.py
@@ -4,5 +4,8 @@ import os
 
 
 # Create a function to implement a ModelCheckpoint callback with a specific filename
-def create_model_checkpoint(model_name):
-  return ModelCheckpoint(filepath=model_name, monitor="val_loss", verbose=0, save_best_only=True)
+def create_model_checkpoint(model_name, save_path="model_experiments"):
+  return ModelCheckpoint(filepath=os.path.join(save_path, model_name),
+                                            monitor="val_loss",
+                                            verbose=0,
+                                            save_best_only=True)
